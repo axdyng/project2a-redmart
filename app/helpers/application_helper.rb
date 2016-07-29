b67724c9c1f7131b7a9d835ef59f7acfc52a109b
Dylan Ng <ngxinde@gmail.com>
2016-07-29 20:46:35 +0800

    added static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def heading_title(heading)
+    base_heading = "RedMart"
+
+    if heading != ''
+      base_heading = base_heading + ' | ' + heading
+    else
+      base_heading
+    end
+  end
 end
