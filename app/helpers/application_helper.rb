03fcf0db6804cb42e80f68a8def8ecaacb2c69b6
Dylan Ng <ngxinde@gmail.com>
2016-07-30 15:41:05 +0800

    added signup page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def heading_title(heading)
-    base_heading = "RedMart"
+    heading
 
-    if heading != ''
-      base_heading = base_heading + ' | ' + heading
-    else
-      base_heading
-    end
+  #   if heading != ''
+  #     base_heading = base_heading + ' | ' + heading
+  #   else
+  #     base_heading
+  #   end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
+  # Static pages routes, not connected to models
+  get '/help',  to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/support',  to: 'static_pages#support'
 
+  # Dynamic routes
+  get '/signup', to: 'users#new'
 
+  # RESTful routes (CRUD)
   resources :users
 end
