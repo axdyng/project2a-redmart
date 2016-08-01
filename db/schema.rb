f177eae08f5f44972c43d1f1f8cb04fea1dc0454
Dylan Ng <ngxinde@gmail.com>
2016-08-02 01:08:31 +0800

    renamed migration create_reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801025101) do
+ActiveRecord::Schema.define(version: 20160801174536) do
 
   create_table "products", force: :cascade do |t|
     t.string   "imgurl"
