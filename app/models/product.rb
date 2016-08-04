40e6860c92d81dceaf17772f35027c18b7388219
Dylan Ng <ngxinde@gmail.com>
2016-08-05 01:35:58 +0800

    edited product model search

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,10 @@ class Product < ApplicationRecord
   validates :name, :price,
             presence: true
 
-  # scope     :search, -> search { where("name LIKE ?", "%#{search}")}
+  scope     :search, -> search { where("name LIKE ?", "%#{search}")}
 
-  def self.search(search)
-    where("name LIKE ?", "%#{search}%")
-  end
+  # def self.search(search)
+  #   where("name LIKE ?", "%#{search}%")
+  # end
 
 end
