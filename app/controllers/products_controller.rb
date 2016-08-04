2698386ef479bd0f3236ee44fb1a4fc56d38e1fe
Dylan Ng <ngxinde@gmail.com>
2016-08-05 01:17:03 +0800

    added one model search function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
-  before_action :find_product, only:  [:show]
+  before_action :find_product, only:  [ :show ]
+
+  def index
+    @products = Product.search(params[:search])
+    if @products.present?
+      flash.now[:success] =  @products.count.to_s + ' Records Found!'
+    else
+      flash.now[:info]    = 'No Records Found!'
+    end
+  end
 
   def show
     @review = current_user.reviews.build if logged_in?
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,10 @@ class Product < ApplicationRecord
   validates :name, :price,
             presence: true
 
+  # scope     :search, -> search { where("name LIKE ?", "%#{search}")}
+
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+  end
+
 end
